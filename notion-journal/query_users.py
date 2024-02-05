092cc09a56512882f62ceba0c1a0f54254b89ea0
V.G. Bulavintsev <golem.md@gmail.com>
2024-02-05 19:45:12 +0100

    Add userlist query script

---
+++ notion-journal/query_users.py
@@ -0,0 +1,28 @@
+import configparser
+from pathlib import Path
+
+import requests
+
+DEFAULT_CONFIG_PATH = Path.home() / "notion_poster.ini"
+
+config = configparser.ConfigParser()
+config.read(DEFAULT_CONFIG_PATH)
+notion_api_key = config['GENERAL']['api_key']
+
+headers = {
+    "Authorization": f"Bearer {notion_api_key}",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json"
+}
+
+response = requests.get('https://api.notion.com/v1/users', headers=headers)
+
+users = response.json().get('results', [])
+
+# print table header
+print("| User ID | Username |")
+print("| --- | --- |")
+
+# print each row
+for user in users:
+    print(f"| {user['id']} | {user['name']} |")
