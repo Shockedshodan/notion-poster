269e14ad91231044382674afe47b720905d4722e
Bastion Psa25 <ur mom>
2024-02-12 15:30:13 +0000

    moved winver into notion_journal

--- notion_journal/notionposter_windows/main.py
+++ notion_journal/notionposter_windows/main.py
@@ -5,7 +5,6 @@ import os
 plugindir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(plugindir)
 sys.path.append(os.path.join(plugindir, "lib"))
-sys.path.append("../notion_journal")
 
 from notion_journal.notion_poster import NotionJournalPoster
 import requests
